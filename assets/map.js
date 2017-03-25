31a7553450d0b713fdb2f0daefafdd5cbe9f4eeb
meeshyd <michellemdidier@gmail.com>
2017-03-25 10:41:30 -0500

    committing new test files for layout and styling

--- assets/map.js
+++ assets/map.js
@@ -3,6 +3,7 @@
         // Create a new StyledMapType object, passing it an array of styles,
         // and the name to be displayed on the map type control.
         var styledMapType = new google.maps.StyledMapType(
+
             [
                 {
                     "featureType": "administrative",
@@ -250,8 +251,9 @@
         // Create a map object, and include the MapTypeId to add
         // to the map type control.
         var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 41.8796, lng: -87.6237},
+          center: {lat: 41.8850, lng: -87.7845},
           zoom: 11,
+          streetViewControl: false,
           mapTypeControlOptions: {
             mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain',
                     'styled_map']
