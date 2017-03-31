c46b8ac326b3222d3e06d058e1ea087fd9fea538
meeshyd <michellemdidier@gmail.com>
2017-03-31 00:22:48 -0500

    fixed some toggling issues with mobile nav. added css to position autocomplete for mobile.

--- assets/front.js
+++ assets/front.js
@@ -283,44 +283,340 @@ function initMap() {
         map.setMapTypeId('styled_map');
     };
 
-var mobileMenu = function() {
-  /* Push the body and the nav over by 285px over */
-  $('#responsive-menu-button').click(function(event) {
-    event.preventDefault();
-    if ($(this).hasClass('in')) {
-        $('.content-panel').animate({
-          left: "0px"
-        }, 200);
+//Initializes Google Maps APIs for autocomplete and the map.
+function initialize() {
+    initAutocomplete();
+    initMap();
+}
 
-        $('body').animate({
-          left: "60%"
-        }, 200);
-        $(this).addClass('out').removeClass('in');
+//Google Autocomplete API for address inputs.
+function initAutocomplete() {
+// Create the autocomplete object, restricting the search to geographical
+// location types.
+    autocomplete = new google.maps.places.Autocomplete(
+    /** @type {!HTMLInputElement} */(document.getElementById('autocomplete')),
+    {types: ['geocode']});
 
-    }else{
-        $('.content-panel').animate({
-            left: "-60%"
-        }, 200);
+    autocomplete2 = new google.maps.places.Autocomplete(
+    /** @type {!HTMLInputElement} */(document.getElementById('autocomplete2')),
+    {types: ['geocode']});
+}
 
-        $('body').animate({
-            left: "0px"
-        }, 200);
-        $(this).addClass('in').removeClass('out');
+//Map function.
+function initMap() {
+
+    var styledMapType = new google.maps.StyledMapType(
+
+        [
+            {
+                "featureType": "administrative",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#6195a0"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative.province",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "lightness": "0"
+                    },
+                    {
+                        "saturation": "0"
+                    },
+                    {
+                        "color": "#f5f5f2"
+                    },
+                    {
+                        "gamma": "1"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape.man_made",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "lightness": "-3"
+                    },
+                    {
+                        "gamma": "1.00"
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape.natural.terrain",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "poi.business",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "hue": "#6fc6ca"
+                    },
+                ]
+            },
+            {
+                "featureType": "poi.restaurant",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "hue": "#6fc6ca"
+                    },
+                ]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "color": "#bae5ce"
+                    },
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "saturation": -100
+                    },
+                    {
+                        "lightness": 45
+                    },
+                    {
+                        "visibility": "simplified"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "simplified"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "color": "#fac9a9"
+                    },
+                    {
+                        "visibility": "simplified"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "labels.text",
+                "stylers": [
+                    {
+                        "color": "#4e4e4e"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#787878"
+                    }
+                ]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "labels.icon",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "simplified"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit.station.airport",
+                "elementType": "labels.icon",
+                "stylers": [
+                    {
+                        "hue": "#0a00ff"
+                    },
+                    {
+                        "saturation": "-77"
+                    },
+                    {
+                        "gamma": "0.57"
+                    },
+                    {
+                        "lightness": "0"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit.station.rail",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "color": "#43321e"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit.station.rail",
+                "elementType": "labels.icon",
+                "stylers": [
+                    {
+                        "hue": "#ff6c00"
+                    },
+                    {
+                        "lightness": "4"
+                    },
+                    {
+                        "gamma": "0.75"
+                    },
+                    {
+                        "saturation": "-68"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "color": "#eaf6f8"
+                    },
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "color": "#c7eced"
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "lightness": "-49"
+                    },
+                    {
+                        "saturation": "-53"
+                    },
+                    {
+                    "gamma": "0.79"
+                    }
+                ]
+            }
+        ],
+        {name: 'Styled Map'});
+
+        // Create a map object, and include the MapTypeId to add
+        // to the map type control.
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 41.8850, lng: -87.7845},
+          zoom: 11,
+          streetViewControl: false,
+          mapTypeControlOptions: {
+            mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain',
+                    'styled_map']
+          }
+        });
+
+        //Associate the styled map with the MapTypeId and set it to display.
+        map.mapTypes.set('styled_map', styledMapType);
+        map.setMapTypeId('styled_map');
     };
-  });
 
-  /* Then push them back */
-  $('#button-submit').click(function(event) {
-    event.preventDefault();
-    $('.content-panel').animate({
-      left: "-60%"
-    }, 200);
+var $menuButton = $('#responsive-menu-button');
+var $buttonSubmit = $('#button-submit')
+var $contentPanel = $('.content-panel');
+var $body = $('body');
 
-    $('body').animate({
-      left: "0px"
-    }, 200);
-  });
+var mobileMenu = function() {
+
+// function to toggle slide menu when hamburger icon is clicked
+    $menuButton.click(function() {
+        
+        $buttonSubmit.addClass('mobile');
+        
+        $('.mobile').click(function() {
+            slideIn();
+        });
+
+        if ($(this).hasClass('in')) {
+            slideOut();
+
+        }else if ($(this).hasClass('out')) {
+            slideIn();
+        };
+        
+        function slideOut() {
+            $contentPanel.animate({
+                  left: "0px"
+                }, 200);
+
+                $body.animate({
+                  left: "60%"
+                }, 200);
+            $menuButton.addClass('out').removeClass('in');
+        };
+        
+        function slideIn() {
+            $contentPanel.animate({
+                    left: "-60%"
+                }, 200);
+
+                $body.animate({
+                    left: "0px"
+                }, 200);
+            $buttonSubmit.removeClass('mobile')
+            $menuButton.addClass('in').removeClass('out');
+        };
+    });  
 };
 
 $(document).ready(mobileMenu);
+
+
 
